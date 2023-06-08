99e4a00704c8541ac206f607fdfe1a787b7fc40c
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-06-08 16:36:04 +0530

    added insert node in binary heap

--- temp.py
+++ temp.py
@@ -36,4 +36,41 @@ def levelOrderTraversal(rootNode):
         print(rootNode.customList[idx])
 
 
+def heapifyTreeInsert(rootNode, index, heapType):
+    parentIndex = int(index/2)
+    if index <= 1:
+        return
+    
+    # Performed element swapping for sorting.
+    if heapType == "Min":
+        if rootNode.customList[index] < rootNode.customList[parentIndex]:
+            temp = rootNode.customList[index]
+            rootNode.customList[index] = rootNode.customList[parentIndex]
+            rootNode.customList[parentIndex] = temp
+            heapifyTreeInsert(rootNode, parentIndex, heapType)
+    if heapType == "Max":
+        if rootNode.customList[index] > rootNode.customList[parentIndex]:
+            temp = rootNode.customList[index]
+            rootNode.customList[index] = rootNode.customList[parentIndex]
+            rootNode.customList[parentIndex] = temp
+            heapifyTreeInsert(rootNode, parentIndex, heapType)
+
+
+"""
+TimeComplexity is O(LogN)
+SpaceComplexity is O(LogN)
+"""
+def insertNode(rootNode, nodeValue, heapType):
+    if rootNode.heapSize + 1 == rootNode.maxSize:
+        return "Binary Heap is full"
+    rootNode.customList[rootNode.heapSize + 1] = nodeValue
+    rootNode.heapSize += 1
+    heapifyTreeInsert(rootNode, rootNode.heapSize, heapType)
+    return "The value is successfully inserted"
+
+
 newBinaryHeap = Heap(5)
+insertNode(newBinaryHeap, 20, "Max")
+insertNode(newBinaryHeap, 10, "Max")
+insertNode(newBinaryHeap, 5, "Max")
+levelOrderTraversal(newBinaryHeap)
