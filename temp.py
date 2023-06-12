055b76f7dd3e7a44c9af303ba7a527772d4c4bff
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-06-12 12:31:53 +0530

    added insert string to trie

--- temp.py
+++ temp.py
@@ -4,6 +4,25 @@ class TrieNode:
         self.endOfString = False
 
 
+"""
+TimeComplexity is O(m)
+SpaceComplexity is O(m)
+"""
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+    
+    def insertString(self, word):
+        current = self.root
+        for ch in word:
+            node = current.children.get(ch)
+            if node == None:
+                node = TrieNode()
+                current.children.update({ch: node})
+            current = node
+        current.endOfString = True
+        print("successfully inserted.")
+
+
+newTrie = Trie()
+newTrie.insertString("API")
