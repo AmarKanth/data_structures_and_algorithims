f5373056a84ed1c7d66839f6ab81b0489ae1a168
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-06-13 18:05:51 +0530

    changes in trie

--- temp.py
+++ temp.py
@@ -37,6 +37,68 @@ class Trie:
         return currentNode.endOfString
 
 
+"""
+If some part of given word is part of the word(API, APPLE)
+     A
+    /
+   P
+  / \
+ I   P
+      \
+       L
+        \
+         E
+
+If given string prefix is part of other string(API, APIS)
+     A
+    /
+   P
+  / \
+ I   P
+  \   \
+   S   L
+        \
+         E
+"""
+def deleteString(root, word, index):
+    ch = word[index]
+    currentNode = root.children.get(ch)
+    canThisNodeBeDeleted = False
+
+    # If the node has morethan one chidren
+    # Loop it to next character
+    if len(currentNode.children) >= 1:
+        deleteString(currentNode, word, index+1)
+        return False
+    
+    # When index at last character in the word
+    if index == len(word) - 1:
+        if len(currentNode.children) >= 1:
+            currentNode.endOfString = False
+            return False
+        else:
+            # Delete Characters will be happen 
+            # from leaf node to first node
+            root.children.pop(ch)
+            return True
+    
+    # If the given word is prefix of other
+    # we cant delete the word
+    if currentNode.endOfString == True:
+        deleteString(currentNode, word, index+1)
+        return False
+    
+    canThisNodeBeDeleted = deleteString(currentNode, word, index+1)
+    if canThisNodeBeDeleted == True:
+        root.children.pop(ch)
+        return True
+    else:
+        return False
+
 newTrie = Trie()
+newTrie.insertString("APPLE")
 newTrie.insertString("API")
-newTrie.searchString("AP")
+
+deleteString(newTrie.root, "API", 0)
+print(newTrie.searchString("API"))
+print(newTrie.searchString("APPLE"))
