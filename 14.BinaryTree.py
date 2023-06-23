7f8d934f3dc4fb336a465f27492ca1e70621644e
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-06-23 18:33:40 +0530

    changes in BT

--- 14.BinaryTree.py
+++ 14.BinaryTree.py
@@ -16,12 +16,10 @@ class TreeNode:
         self.children.append(TreeNode)
 
 tree = TreeNode("Drinks", [])
-
 cold = TreeNode("Cold", [])
 hot = TreeNode("Hot", [])
 tree.addChild(cold)
 tree.addChild(hot)
-
 tea = TreeNode("Tea", [])
 coffee = TreeNode("Coffee", [])
 cola = TreeNode("Cola", [])
@@ -30,29 +28,12 @@ cold.addChild(cola)
 cold.addChild(fanta)
 hot.addChild(tea)
 hot.addChild(coffee)
-
 print(tree)
 
 
 """
-Types Of BinaryTree
-
-1. Full Binary Tree     :-  Each node should have 2 children or zero children
-2. Perfect Binary Tree  :-  All the non-leaf nodes has two children and are at the same level(depth), 
-                            all leaf nodes has zero children and are the same level(depth)
-3. Complete Binary Tree :-  All levels are completely filled except last level.
-4. Balanced Binary Tree :-  All the leaf nodes are in same level(depth)
+Binary Tree With List
 """
-
-"""
-Types of Traversal Methods
-
-preorder    :- All-LeftNodes --> All-RightNodes
-inorder     :- LeftNode --> RootNode --> RightNode
-postorder   :- LeftNode --> RightNode --> RootNode
-levelorder  :- Depth-1 --> Depth-2 --- Till-LastNode
-"""
-
 class BinaryTree:
     """
     TimeComplexity is O(1)
@@ -153,7 +134,6 @@ newBT.insertNode("Hot")
 newBT.insertNode("Cold")
 newBT.insertNode("Tea")
 newBT.insertNode("Coffee")
-newBT.deleteBT()
 
 
 """
