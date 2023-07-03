8427b481e2c8eba95a4b24be4f736e3916a7d338
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-07-03 18:31:37 +0530

    Changes in MergeSort

--- temp.py
+++ temp.py
@@ -1,42 +1,52 @@
-import math
+"""
+1. Merge Sort is a devide and conquer algorithm
+2. Divide the input array in two halves and we keep halving recusrively 
+until they become too small that cant not be broken further
+3. Merge halves by sorting them
+"""
 
-def insertSort(customList):
-    for i in range(1, len(customList)):
-        key = customList[i]
-        j = i - 1 
-        while j >= 0 and key < customList[j]:
-            customList[j+1] = customList[j]
-            j -= 1
-        customList[j+1] = key
+def merge(customList, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = [0]*(n1)
+    R = [0]*(n2)
+
+    for i in range(0, n1):
+        L[i] = customList[l+i]
+    
+    for j in range(0, n2):
+        R[j] = customList[m+1+j]
+    
+    i = 0
+    j = 0
+    k = l
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            customList[k] = L[i]
+            i += 1
+        else:
+            customList[k] = R[j]
+            j += 1
+        k += 1
+    while i < n1:
+        customList[k] = L[i]
+        i += 1
+        k += 1
+    while j < n2:
+        customList[k] = R[j]
+        j += 1
+        k += 1
+
+def mergeSort(customList, l, r):
+    if l < r:
+        m = (l+(r-1))//2
+        mergeSort(customList, l, m)
+        mergeSort(customList, m+1, r)
+        merge(customList, l, m, r)
     return customList
 
-"""
-TimeComplexity is O(n^2)
-SpaceComplexity is O(1)
-"""
-def buckerSort(customList):
-    numberOfBuckets = round(math.sqrt(len(customList)))
-    maxValue = max(customList)
-    arr = []
-
-    for i in range(numberOfBuckets):
-        arr.append([])
-    
-    for j in customList:
-        bucket_idx = math.ceil(j*numberOfBuckets/maxValue)
-        arr[bucket_idx-1].append(j)
-    
-    for i in range(numberOfBuckets):
-        arr[i] = insertSort(arr[i])
-    
-    k = 0
-    for i in range(numberOfBuckets):
-        for j in range(len(arr[i])):
-            customList[k] = arr[i][j]
-            k += 1
-    return customList
-
-
-cList = [2,1,5,3,9,7,4,6,8]
-res = buckerSort(cList)
+cList = [2,1,7,6,5,3,4,9,8]
+res = mergeSort(cList, 0, 8)
 print(res)
