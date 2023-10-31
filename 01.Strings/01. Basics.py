003dbbdb91d896c496bca3091cc12501129af92c
AmarKanth <amarkanth.bollu@gmail.com>
2023-10-31 08:00:06 +0530

    changes in strings

---
+++ 01.Strings/01. Basics.py
@@ -0,0 +1,33 @@
+"""
+upper - Converts to upper
+isupper - checks the given string is uppercase.
+"""
+
+"""
+lower - Converts to lower
+islower - checks the given string is lowercase.
+"""
+
+"""
+capitalize - it capitalizes first letter in the string.
+"""
+
+"""
+split - it converts string into list based on delimiter.
+"""
+
+"""
+count - it counts the number occurrences of specific element.
+"""
+
+"""
+find - it verifies specific element in the string. Returns –1 if it doesn’t have given element.
+"""
+
+"""
+isalnum - checks the given string is alphanumeric.
+"""
+
+"""
+isalpha - checks the given string is alpha.
+"""
