5d3e1a083814bf91186e3e2aedcabe6ca7418176
AmarKanth <amarkanth.bollu@gmail.com>
2023-10-31 08:27:26 +0530

    changes in sets

--- 06.Set/06.Set.py
+++ 06.Set/06.Set.py
@@ -32,3 +32,43 @@ s1 = set("ac")
 s2 = set("ab")
 res = s1.intersection(s2)
 print(res)
+
+"""
+copy - it copies the original set and changes in original won’t change the copied object.
+"""
+
+"""
+difference - it returns the difference of the two sets.
+"""
+
+"""
+difference_update - it removes the items that exists in both the sets.
+"""
+
+"""
+intersection_update - it removes the non-common items between the sets.
+"""
+
+"""
+isdisjoint - it returns true if the sets have null intersection and vice versa.
+"""
+
+"""
+issubset - it returns true if the set is subset of other set.
+"""
+
+"""
+issuperset - it’s reverse process of subset (if set contains other set).
+"""
+
+"""
+symetric_difference - it returns non common items.
+"""
+
+"""
+union - it combines the two sets.
+"""
+
+"""
+update - it updates set with non-existing items.
+"""
