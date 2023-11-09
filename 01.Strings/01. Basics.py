27dac02d1631fe882a689cc06dcaf112d5c114a1
AmarKanth <amarkanth.bollu@gmail.com>
2023-11-09 08:16:13 +0530

    changes in strings

--- 01.Strings/01. Basics.py
+++ 01.Strings/01. Basics.py
@@ -21,13 +21,22 @@ count - it counts the number occurrences of specific element.
 """
 
 """
-find - it verifies specific element in the string. Returns –1 if it doesn’t have given element.
+find - it verifies specific prase in the string. Returns –1 if it doesn’t have given element.
 """
 
 """
-isalnum - checks the given string is alphanumeric.
+isalnum - method in Python is used to check if all the characters in a given string are alphanumeric. 
+Alphanumeric characters include letters (alphabets) and numbers (digits). If all the characters in the string 
+are either letters or numbers (and the string is not empty), the isalnum() method returns True; 
+otherwise, it returns False.
 """
+lst = ["HelloWorld", "Hello, World!", "12345", "", "Hello123"]
+res = [ele for ele in lst if ele.isalnum()]
+print(res)
+
 
 """
-isalpha - checks the given string is alpha.
+isalpha - method is used to check if all the characters in a given string are alphabetic characters (letters) 
+and there are no numeric or special characters in the string. It returns True if all characters in the string 
+are alphabetic, and False otherwise.
 """
---
+++ temp.py
@@ -0,0 +1,2 @@
+name = "Amar"
+print(name.upper())
