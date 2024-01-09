3d266370233085452e48c427a6736b981b9261bb
AmarKanth <amarkanth.bollu@gmail.com>
2024-01-09 23:11:03 +0530

    changes in test

--- test.py
+++ test.py
@@ -15,19 +15,19 @@ def get_balance(root_node):
         return 0
     return get_height(root_node.left_child) - get_height(root_node.right_child)
 
-def right_rotate(disbalanced_node):
-    new_root = disbalanced_node.left_child
-    disbalanced_node.left_child = disbalanced_node.left_child.right_child
-    new_root.right_child = disbalanced_node
-    disbalanced_node.height = 1 + max(get_height(disbalanced_node.left_child), get_height(disbalanced_node.right_child))
+def right_rotate(disbalance_node):
+    new_root = disbalance_node.left_child
+    disbalance_node.left_child = disbalance_node.left_child.right_child
+    new_root.right_child = disbalance_node
+    disbalance_node.height = 1 + max(get_height(disbalance_node.left_child), get_height(disbalance_node.right_child))
     new_root.height = 1 + max(get_height(new_root.left_child), get_height(new_root.right_child))
     return new_root
 
-def left_rotate(disbalanced_node):
-    new_root = disbalanced_node.right_child
-    disbalanced_node.right_child = disbalanced_node.right_child.left_child
-    new_root.left_child = disbalanced_node
-    disbalanced_node.height = 1 + max(get_height(disbalanced_node.left_child), get_height(disbalanced_node.right_child))
+def left_rotate(disbalance_node):
+    new_root = disbalance_node.right_child
+    disbalance_node.right_child = disbalance_node.right_child.left_child
+    new_root.left_child = disbalance_node
+    disbalance_node.height = 1 + max(get_height(disbalance_node.left_child), get_height(disbalance_node.right_child))
     new_root.height = 1 + max(get_height(new_root.left_child), get_height(new_root.right_child))
     return new_root
 
@@ -38,31 +38,46 @@ def insert_node(root_node, node_value):
         root_node.left_child = insert_node(root_node.left_child, node_value)
     else:
         root_node.right_child = insert_node(root_node.right_child, node_value)
-
+    
     root_node.height = 1 + max(get_height(root_node.left_child), get_height(root_node.right_child))
     balance = get_balance(root_node)
-
-    # if balance > 1 and get_balance(root_node.left_child) >= 0:
-    #     return right_rotate(root_node)
-    # if balance > 1 and get_balance(root_node.left_child) < 0:
-    #     root_node.left_child = left_rotate(root_node.left_child)
-    #     return right_rotate(root_node)
-    # if balance < -1 and get_balance(root_node.right_child) <= 0:
-    #     return left_rotate(root_node)
-    # if balance < -1 and get_balance(root_node.right_child) > 0:
-    #     root_node.right_child = right_rotate(root_node.right_child)
-    #     return left_rotate(root_node)
+    if balance > 1 and get_balance(root_node.left_child) >= 0:
+        return right_rotate(root_node)
+    if balance > 1 and get_balance(root_node.left_child) < 0:
+        root_node.left_child = left_rotate(root_node.left_child)
+        return right_rotate(root_node)
+    if balance < -1 and get_balance(root_node.right_child) <= 0:
+        return left_rotate(root_node)
+    if balance < -1 and get_balance(root_node.right_child) > 0:
+        root_node.right_child = right_rotate(root_node.right_child)
+        return left_rotate(root_node)
     return root_node
+
+def search_node(root_node, value):
+    if root_node.data == value:
+        print("value is found")
+    elif value < root_node.data:
+        search_node(root_node.left_child)
+    else:
+        search_node(root_node.right_child)
+
+def get_min_value_node(root_node):
+    if root_node is None or root_node.left_child is None:
+        return root_node
+    return get_min_value_node(root_node.left_child)
+
+def delete_node(root_node, value):
+    pass
 
 def print_tree(root_node, level=0):
     if root_node:
         print_tree(root_node.left_child, level+1)
-        print(" " * 4 * level + "->"+str(root_node.data))
+        print(" " * 4 * level + "->" + str(root_node.data))
         print_tree(root_node.right_child, level+1)
 
 avl = AVLNode(30)
-insert_node(avl, 20)
 insert_node(avl, 40)
-insert_node(avl, 60)
-insert_node(avl, 55)
+insert_node(avl, 20)
+insert_node(avl, 50)
+insert_node(avl, 45)
 print_tree(avl)
