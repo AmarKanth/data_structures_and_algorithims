2f99a382f7088a663d128e4e7956de6eb43aab7b
AmarKanth <amarkanth.bollu@gmail.com>
2024-01-17 19:27:50 +0530

    changes in binary heap

--- 12. tree/06.binary_heap.py
+++ 12. tree/06.binary_heap.py
@@ -1,139 +1,124 @@
+"""
+                        TimeComplexity      SpaceComplexity
+peak_of_heap            O(1)                O(1)
+size_of_heap            O(1)                O(1)
+level_order_traversal   O(n)                O(1)
+insert_node             O(logN)             O(logN)
+extract_node            O(logN)             O(logN)
+delete_bh               O(1)                O(1)
+"""
 class Heap:
     def __init__(self, size):
-        self.customList = (size+1) * [None]
-        self.heapSize = 0
-        self.maxSize = size + 1
-    
-"""
-TimeComplexity is O(1)
-SpaceComplexity is O(1)
-"""
-def peakofHeap(rootNode):
-    if not rootNode:
-        return
-    return rootNode.customList[1]
+        self.custom_list = (size+1) * [None]
+        self.heap_size = 0
+        self.max_size = size + 1
 
-"""
-TimeComplexity is O(1)
-SpaceComplexity is O(1)
-"""
-def sizeofHeap(rootNode):
-    if not rootNode:
+def peak_of_heap(root_node):
+    if not root_node:
         return
-    return rootNode.heapSize
+    return root_node.custom_list[1]
 
-"""
-TimeComplexity is O(n)
-SpaceComplexity is O(1)
-"""
-def levelOrderTraversal(rootNode):
-    if not rootNode:
+def size_of_heap(root_node):
+    if not root_node:
         return
-    for idx in range(1, rootNode.heapSize+1):
-        print(rootNode.customList[idx])
+    return root_node.heap_size
 
-def heapifyTreeInsert(rootNode, index, heapType):
-    parentIndex = int(index/2)
+def level_order_traversal(root_node):
+    if not root_node:
+        return
+    for idx in range(1, root_node.heap_size+1):
+        print(root_node.custom_list[idx])
+
+def heapify_tree_insert(root_node, index, heap_type):
+    parent_index = int(index/2)
     if index <= 1:
         return
     
     # Performed element swapping for sorting.
-    if heapType == "Min":
-        if rootNode.customList[index] < rootNode.customList[parentIndex]:
-            temp = rootNode.customList[index]
-            rootNode.customList[index] = rootNode.customList[parentIndex]
-            rootNode.customList[parentIndex] = temp
-            heapifyTreeInsert(rootNode, parentIndex, heapType)
-    if heapType == "Max":
-        if rootNode.customList[index] > rootNode.customList[parentIndex]:
-            temp = rootNode.customList[index]
-            rootNode.customList[index] = rootNode.customList[parentIndex]
-            rootNode.customList[parentIndex] = temp
-            heapifyTreeInsert(rootNode, parentIndex, heapType)
+    if heap_type == "Min":
+        if root_node.custom_list[index] < root_node.custom_list[parent_index]:
+            temp = root_node.custom_list[index]
+            root_node.custom_list[index] = root_node.custom_list[parent_index]
+            root_node.custom_list[parent_index] = temp
+            heapify_tree_insert(root_node, parent_index, heap_type)
+    if heap_type == "Max":
+        if root_node.custom_list[index] > root_node.custom_list[parent_index]:
+            temp = root_node.custom_list[index]
+            root_node.custom_list[index] = root_node.custom_list[parent_index]
+            root_node.custom_list[parent_index] = temp
+            heapify_tree_insert(root_node, parent_index, heap_type)
 
-"""
-TimeComplexity is O(LogN)
-SpaceComplexity is O(LogN)
-"""
-def insertNode(rootNode, nodeValue, heapType):
-    if rootNode.heapSize + 1 == rootNode.maxSize:
+def insert_node(root_node, node_value, heap_type):
+    if root_node.heap_size + 1 == root_node.max_size:
         return "Binary Heap is full"
-    rootNode.customList[rootNode.heapSize + 1] = nodeValue
-    rootNode.heapSize += 1
+    root_node.custom_list[root_node.heap_size + 1] = node_value
+    root_node.heap_size += 1
     # Heapifying will be performed from last node to rootnode
-    heapifyTreeInsert(rootNode, rootNode.heapSize, heapType)
+    heapify_tree_insert(root_node, root_node.heap_size, heap_type)
     return "The value is successfully inserted"
 
-def heapifyTreeExtract(rootNode, index, heapType):
-    leftIndex = index * 2
-    rightIndex = index * 2 + 1
-    swapChild = 0
+def heapify_tree_extract(root_node, index, heap_type):
+    left_index = index * 2
+    right_index = index * 2 + 1
+    swap_child = 0
 
-    if rootNode.heapSize < leftIndex:
+    if root_node.heap_size < left_index:
         return 
     # If current node has only leftchild and leftchild is the last child
-    elif rootNode.heapSize == leftIndex:
-        if heapType == "Min":
-            if rootNode.customList[index] > rootNode.customList[leftIndex]:
-                temp = rootNode.customList[index]
-                rootNode.customList[index] = rootNode.customList[leftIndex]
-                rootNode.customList[leftIndex] = temp
+    elif root_node.heap_size == left_index:
+        if heap_type == "Min":
+            if root_node.custom_list[index] > root_node.custom_list[left_index]:
+                temp = root_node.custom_list[index]
+                root_node.custom_list[index] = root_node.custom_list[left_index]
+                root_node.custom_list[left_index] = temp
             return
         else:
-            if rootNode.customList[index] < rootNode.customList[leftIndex]:
-                temp = rootNode.customList[index]
-                rootNode.customList[index] = rootNode.customList[leftIndex]
-                rootNode.customList[leftIndex] = temp
+            if root_node.custom_list[index] < root_node.custom_list[left_index]:
+                temp = root_node.custom_list[index]
+                root_node.custom_list[index] = root_node.custom_list[left_index]
+                root_node.custom_list[left_index] = temp
             return
     # If current node has right and left child
     else:
-        if heapType == "Min":
+        if heap_type == "Min":
             # Find Min value between left and right childs
-            if rootNode.customList[leftIndex] < rootNode.customList[rightIndex]:
-                swapChild = leftIndex
+            if root_node.custom_list[left_index] < root_node.custom_list[right_index]:
+                swap_child = left_index
             else:
-                swapChild = rightIndex
-            temp = rootNode.customList[index]
-            rootNode.customList[index] = rootNode.customList[swapChild]
-            rootNode.customList[swapChild] = temp
+                swap_child = right_index
+            temp = root_node.custom_list[index]
+            root_node.custom_list[index] = root_node.custom_list[swap_child]
+            root_node.custom_list[swap_child] = temp
         else:
             # Find Max value between left and right childs
-            if rootNode.customList[leftIndex] > rootNode.customList[rightIndex]:
-                swapChild = leftIndex
+            if root_node.custom_list[left_index] > root_node.custom_list[right_index]:
+                swap_child = left_index
             else:
-                swapChild = rightIndex
-            temp = rootNode.customList[index]
-            rootNode.customList[index] = rootNode.customList[swapChild]
-            rootNode.customList[swapChild] = temp
-    heapifyTreeExtract(rootNode, swapChild, heapType)
+                swap_child = right_index
+            temp = root_node.custom_list[index]
+            root_node.custom_list[index] = root_node.custom_list[swap_child]
+            root_node.custom_list[swap_child] = temp
+    heapify_tree_extract(root_node, swap_child, heap_type)
 
-"""
-TimeComplexity is O(LogN)
-SpaceComplexity is O(LogN)
-"""
-def extractNode(rootNode, heapType):
-    if rootNode.heapSize == 0:
+def extract_node(root_node, heap_type):
+    if root_node.heap_size == 0:
         return
     else:
         # Binary Heap allowed to extract rootNode
-        extractedNode = rootNode.customList[1]
-        rootNode.customList[1] = rootNode.customList[rootNode.heapSize]
-        rootNode.customList[rootNode.heapSize] = None
-        rootNode.heapSize -= 1
+        extracted_node = root_node.custom_list[1]
+        root_node.custom_list[1] = root_node.custom_list[root_node.heap_size]
+        root_node.custom_list[root_node.heap_size] = None
+        root_node.heap_size -= 1
         # Heapifying will be performed from rootNode
-        heapifyTreeExtract(rootNode, 1, heapType)
-        return extractedNode
+        heapify_tree_extract(root_node, 1, heap_type)
+        return extracted_node
     
-"""
-TimeComplexity is O(1)
-SpaceComplexity is O(1)
-"""
-def deleteBH(rootNode):
-    rootNode.customList = None
+def delete_bh(root_node):
+    root_node.custom_list = None
 
-newBinaryHeap = Heap(5)
-insertNode(newBinaryHeap, 4, "Max")
-insertNode(newBinaryHeap, 5, "Max")
-insertNode(newBinaryHeap, 2, "Max")
-insertNode(newBinaryHeap, 1, "Max")
-levelOrderTraversal(newBinaryHeap)
+new_binary_heap = Heap(5)
+insert_node(new_binary_heap, 4, "Max")
+insert_node(new_binary_heap, 5, "Max")
+insert_node(new_binary_heap, 2, "Max")
+insert_node(new_binary_heap, 1, "Max")
+level_order_traversal(new_binary_heap)
