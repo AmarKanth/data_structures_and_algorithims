3fe3dadc6e181c0e356f31dc8494070a1a63d4ec
AmarKanth <amarkanth.bollu@gmail.com>
2024-03-06 13:58:24 +0530

    changes in sll

--- 09. linked list/08.single_linked_list.py
+++ 09. linked list/08.single_linked_list.py
@@ -10,7 +10,6 @@ class Node:
 """
             TimeComplexity      SpaceComplexity
 insert      O(n)                O(1)
-traverse    O(n)                O(1)
 search      O(n)                O(1)
 delete      O(n)                O(1)
 delete_all  O(1)                O(1)
@@ -26,89 +25,85 @@ class SLinkedList:
             yield node
             node = node.next
     
-    def insert(self, value, location):
+    def insert(self, location, value):
         new_node = Node(value)
         if self.head == None:
             self.head = new_node
             self.tail = new_node
-        else:
-            if location == 0:
-                new_node.next = self.head
-                self.head = new_node
-            elif location == -1:
-                new_node.next = None
+            return "Node successfully inserted"
+
+        if location == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return "Node successfully inserted"
+        
+        if location != 0:
+            temp_node = self.head
+            index = 0
+            while index < location - 1:
+                temp_node = temp_node.next
+                index += 1
+            if self.tail == temp_node:
                 self.tail.next = new_node
                 self.tail = new_node
             else:
-                temp_node = self.head
-                index = 0
-                while index < location - 1:
-                    if temp_node.next:
-                        temp_node = temp_node.next
-                    index += 1
                 next_node = temp_node.next
                 temp_node.next = new_node
                 new_node.next = next_node
-                if temp_node == self.tail:
-                    self.tail = new_node
-
-    def traverse(self):
-        if self.head == None:
-            print("The Singly Linked List does not exist")
-        else:
-            temp_node = self.head
-            while temp_node != None:
-                print(temp_node.value)
-                temp_node = temp_node.next
+            return "Node successfully inserted"
     
     def search(self, value):
         if self.head == None:
-           return "The ssl does not exist"
-        else:
-            temp_node = self.head
-            while temp_node != None:
-                if temp_node.value == value:
-                    return temp_node.value
-                temp_node = temp_node.next
-            return "The value does not exist in ssl"
+           return "SLL does not exist"
+
+        temp_node = self.head
+        while temp_node:
+            if temp_node.value == value:
+                return temp_node.value
+            temp_node = temp_node.next
+        return "The value does not exist in ssl"
     
     def delete(self, location):
         if self.head == None:
-            print("The ssl does not exist")
-        else:
-            if location == 0:
-                if self.head == self.tail:
-                    self.head = None
-                    self.tail = None
-                else:
-                    self.head = self.head.next
-            elif location == -1:
-                if self.head == self.tail:
-                    self.head = None
-                    self.tail = None
-                else:
-                    temp_node = self.head
-                    while temp_node != None:
-                        if temp_node.next == self.tail:
-                            break
-                        temp_node = temp_node.next
-                    temp_node.next = None
-                    self.tail = temp_node
-            else:
-                temp_node = self.head
-                index = 0
-                while index < location - 1:
-                    temp_node = temp_node.next
-                    index += 1
-                next_node = temp_node.next
-                temp_node.next = next_node.next
-    
-    def delete_all(self):
-        if self.head is None:
-            print("The ssl does not exist")
-        else:
+            return "SLL is doesnt exist"
+
+        if self.head == self.tail:
             self.head = None
             self.tail = None
+            return "Node successfully deleted from sll"
+        
+        if location == 0:
+            self.head = self.head.next
+            return "Node successfully deleted from sll"
+        
+        if location != 0:
+            temp_node = self.head
+            index = 0
+            while index < location - 1:
+                temp_node = temp_node.next
+                index += 1
+            if self.tail == temp_node:
+                temp_node.next = None
+                self.tail = temp_node
+            else:
+                next_node = temp_node.next
+                temp_node.next = next_node.next
+            return "Node successfully deleted from sll"
+    
+    def delete_all(self):
+        if self.head == None:
+            return "SLL does not exist"
+        self.head = None
+        self.tail = None
+        return "SLL is successfully deleted"
+
+def print_sll(sll):
+    res = []
+    for node in sll:
+        value = node.value if node else None
+        next = node.next.value if node.next else None
+        res.append((value, next))
+    return res
 
 ssl = SLinkedList()
 ssl.insert(1, 1)
@@ -117,7 +112,5 @@ ssl.insert(3, 1)
 ssl.insert(4, 1)
 ssl.insert(0, 0)
 ssl.insert(0, 3)
-print([node.value for node in ssl])
-ssl.traverse()
 search = ssl.search(10)
 print(f"search value is {search}")
