d106a40983b97fe26e7c06ebe9f333b9add5c114
AmarKanth <amarkanth.bollu@gmail.com>
2024-04-30 14:59:12 +0530

    Update 03.insertion_sort.py

--- 15. sort algorithms/03.insertion_sort.py
+++ 15. sort algorithms/03.insertion_sort.py
@@ -11,11 +11,9 @@ def insertion_sort(custom_list):
     for i in range(1, len(custom_list)):
         key = custom_list[i]
         j = i - 1
-        # This loop will continue, until all the elements in sorted array gets sorted.
         while j>=0 and key < custom_list[j]:
             custom_list[j+1] = custom_list[j]
             j -= 1
-        # This will decide the position of the current element in sorted array
         custom_list[j+1] = key
     return custom_list
 
