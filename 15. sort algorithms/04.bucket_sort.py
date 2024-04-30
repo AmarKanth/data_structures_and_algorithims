4e0ad9c3d53919a4b4742aaffcd19f268ea90d10
AmarKanth <amarkanth.bollu@gmail.com>
2024-04-30 18:34:30 +0530

    changes in bucket sort

--- 15. sort algorithms/04.bucket_sort.py
+++ 15. sort algorithms/04.bucket_sort.py
@@ -6,43 +6,43 @@
 
 import math
 
-def insertSort(customList):
-    for i in range(1, len(customList)):
-        key = customList[i]
+def insert_sort(custom_list):
+    for i in range(1, len(custom_list)):
+        key = custom_list[i]
         j = i - 1 
-        while j >= 0 and key < customList[j]:
-            customList[j+1] = customList[j]
+        while j >= 0 and key < custom_list[j]:
+            custom_list[j+1] = custom_list[j]
             j -= 1
-        customList[j+1] = key
-    return customList
+        custom_list[j+1] = key
+    return custom_list
 
 """
 TimeComplexity is O(n^2)
 SpaceComplexity is O(1)
 """
-def buckerSort(customList):
-    numberOfBuckets = round(math.sqrt(len(customList)))
-    maxValue = max(customList)
+def bucket_sort(custom_list):
+    number_of_buckets = round(math.sqrt(len(custom_list)))
+    max_value = max(custom_list)
     arr = []
 
-    for i in range(numberOfBuckets):
+    for i in range(number_of_buckets):
         arr.append([])
     
-    for j in customList:
-        bucket_idx = math.ceil(j*numberOfBuckets/maxValue)
+    for j in custom_list:
+        bucket_idx = math.ceil(j*number_of_buckets/max_value)
         arr[bucket_idx-1].append(j)
     
-    for i in range(numberOfBuckets):
-        arr[i] = insertSort(arr[i])
+    for i in range(number_of_buckets):
+        arr[i] = insert_sort(arr[i])
     
     k = 0
-    for i in range(numberOfBuckets):
+    for i in range(number_of_buckets):
         for j in range(len(arr[i])):
-            customList[k] = arr[i][j]
+            custom_list[k] = arr[i][j]
             k += 1
-    return customList
+    return custom_list
 
 
-cList = [2,1,5,3,9,7,4,6,8]
-res = buckerSort(cList)
+c_list = [2,1,5,3,9,7,4,6,8]
+res = bucket_sort(c_list)
 print(res)
