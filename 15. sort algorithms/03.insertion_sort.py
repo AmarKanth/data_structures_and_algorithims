dc437926404e7657010544dc19321b0ac8511b13
AmarKanth <amarkanth.bollu@gmail.com>
2024-04-30 14:59:38 +0530

    Update 03.insertion_sort.py

--- 15. sort algorithms/03.insertion_sort.py
+++ 15. sort algorithms/03.insertion_sort.py
@@ -11,7 +11,7 @@ def insertion_sort(custom_list):
     for i in range(1, len(custom_list)):
         key = custom_list[i]
         j = i - 1
-        while j>=0 and key < custom_list[j]:
+        while j >= 0 and key < custom_list[j]:
             custom_list[j+1] = custom_list[j]
             j -= 1
         custom_list[j+1] = key
