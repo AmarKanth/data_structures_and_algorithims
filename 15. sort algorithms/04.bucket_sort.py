7d5d76eb6dc99389e5742ad4bbf4a5572bbd799f
AmarKanth <amarkanth.bollu@gmail.com>
2024-05-01 10:14:44 +0530

    added bucket sort with -ve numbers

--- 15. sort algorithms/04.bucket_sort.py
+++ 15. sort algorithms/04.bucket_sort.py
@@ -46,3 +46,33 @@ def bucket_sort(custom_list):
 c_list = [2,1,5,3,9,7,4,6,8]
 res = bucket_sort(c_list)
 print(res)
+
+
+"""
+Bucket sort with negative numbers
+"""
+def bucket_sort(custom_list):
+    number_of_buckets = round(math.sqrt(len(custom_list)))
+    min_value = min(custom_list)
+    max_value = max(custom_list)
+    range_val = (max_value - min_value) / number_of_buckets
+ 
+    buckets = [[] for _ in range(number_of_buckets)]
+ 
+    for j in custom_list:
+        if j == max_value:
+            buckets[-1].append(j)
+        else:
+            index_b = math.floor((j - min_value) / range_val)
+            buckets[index_b].append(j)
+    
+    sorted_array = []
+    for i in range(number_of_buckets):
+        buckets[i] = insertion_sort(buckets[i])
+        sorted_array.extend(buckets[i])
+    
+    return sorted_array
+
+c_list = [2,1,5,3,9,7,4,6,8]
+res = bucket_sort(c_list)
+print(res)
