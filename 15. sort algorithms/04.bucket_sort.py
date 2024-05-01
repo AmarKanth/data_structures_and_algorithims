70516626d7d9fde83fe3b7cb1355975e677252b7
AmarKanth <amarkanth.bollu@gmail.com>
2024-05-01 12:49:31 +0530

    bucket sort added time complexity

--- 15. sort algorithms/04.bucket_sort.py
+++ 15. sort algorithms/04.bucket_sort.py
@@ -18,38 +18,7 @@ def insert_sort(custom_list):
 
 """
 TimeComplexity is O(n^2)
-SpaceComplexity is O(1)
-"""
-def bucket_sort(custom_list):
-    number_of_buckets = round(math.sqrt(len(custom_list)))
-    max_value = max(custom_list)
-    arr = []
-
-    for i in range(number_of_buckets):
-        arr.append([])
-    
-    for j in custom_list:
-        bucket_idx = math.ceil(j*number_of_buckets/max_value)
-        arr[bucket_idx-1].append(j)
-    
-    for i in range(number_of_buckets):
-        arr[i] = insert_sort(arr[i])
-    
-    k = 0
-    for i in range(number_of_buckets):
-        for j in range(len(arr[i])):
-            custom_list[k] = arr[i][j]
-            k += 1
-    return custom_list
-
-
-c_list = [2,1,5,3,9,7,4,6,8]
-res = bucket_sort(c_list)
-print(res)
-
-
-"""
-Bucket sort with negative numbers
+SpaceComplexity is O(n+k)
 """
 def bucket_sort(custom_list):
     number_of_buckets = round(math.sqrt(len(custom_list)))
