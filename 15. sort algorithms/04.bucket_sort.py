5f8204b996cbc6cb936f3b954c7fcf6df703b5fb
AmarKanth <amarkanth.bollu@gmail.com>
2024-07-05 11:17:04 +0530

    Update 04.bucket_sort.py

--- 15. sort algorithms/04.bucket_sort.py
+++ 15. sort algorithms/04.bucket_sort.py
@@ -6,15 +6,15 @@
 
 import math
 
-def insert_sort(custom_list):
-    for i in range(1, len(custom_list)):
-        key = custom_list[i]
-        j = i - 1 
-        while j >= 0 and key < custom_list[j]:
-            custom_list[j+1] = custom_list[j]
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
             j -= 1
-        custom_list[j+1] = key
-    return custom_list
+        arr[j+1] = key
+    return arr
 
 """
 TimeComplexity is O(n^2)
@@ -24,24 +24,19 @@ def bucket_sort(custom_list):
     number_of_buckets = round(math.sqrt(len(custom_list)))
     min_value = min(custom_list)
     max_value = max(custom_list)
-    range_val = (max_value - min_value) / number_of_buckets
- 
+    range_val = (min_value + max_value)/number_of_buckets
     buckets = [[] for _ in range(number_of_buckets)]
- 
-    for j in custom_list:
-        if j == max_value:
-            buckets[-1].append(j)
-        else:
-            index_b = math.floor((j - min_value) / range_val)
-            buckets[index_b].append(j)
-    
     sorted_array = []
+
+    for j in custom_list:
+        idx = math.floor((j-min_value)/range_val)
+        buckets[idx].append(j)
+
     for i in range(number_of_buckets):
-        buckets[i] = insertion_sort(buckets[i])
-        sorted_array.extend(buckets[i])
-    
+        arr = insertion_sort(buckets[i])
+        sorted_array.extend(arr)
+
     return sorted_array
 
-c_list = [2,1,5,3,9,7,4,6,8]
-res = bucket_sort(c_list)
+res = bucket_sort([2,1,5,3,9,7,4,6,8])
 print(res)
