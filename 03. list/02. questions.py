1043557769f2d2fb8272f2a40e1a806a87d4617c
AmarKanth <amarkanth.bollu@gmail.com>
2024-07-15 10:04:06 +0530

    Update 02. questions.py

--- 03. list/02. questions.py
+++ 03. list/02. questions.py
@@ -109,3 +109,21 @@ def find_missing_number(l, n):
     return sum1-sum2
 res = find_missing_number(l, 9)
 print(res)
+
+
+"""
+Find the subsets of given list 
+nums = [1,3]
+"""
+def find_subsets(nums):
+    def backtrack(start, path):
+        subsets.append(path)
+        for i in range(start, len(nums)):
+            backtrack(i + 1, path + [nums[i]])
+
+    subsets = []
+    backtrack(0, [])
+    return subsets
+
+nums = [1, 3]
+print(find_subsets(nums))
