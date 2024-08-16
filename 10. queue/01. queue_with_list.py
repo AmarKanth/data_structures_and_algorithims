1a1c3e03448d6346eb78cd754c2c812f932a178c
AmarKanth <amarkanth.bollu@gmail.com>
2024-08-16 16:29:25 +0530

    Update 01. queue_with_list.py

--- 10. queue/01. queue_with_list.py
+++ 10. queue/01. queue_with_list.py
@@ -4,33 +4,33 @@ queue is datastracure that store items in FIFO(first in first out) manner.
 
 class Queue:
     def __init__(self):
-        self.items = []
+        self.elements = []
     
     def __str__(self):
-        values = [str(i) for i in self.items]
+        values = [str(i) for i in self.elements]
         return " ".join(values)
     
     def is_empty(self):
-        if self.items == []:
+        if self.elements == []:
             return True
         return False
     
     def enqueue(self, value):
-        self.items.append(value)
-        return self.items
+        self.elements.append(value)
+        return self.elements
     
     def dequeue(self):
         if self.is_empty():
             return "Queue is empty"
-        return self.items.pop(0)
+        return self.elements.pop(0)
     
     def peek(self):
         if self.is_empty():
             return "Queue is empty"
-        return self.items[0]
+        return self.elements[0]
     
     def delete(self):
-        self.items = None
+        self.elements = None
 
 q = Queue()
 print(q.is_empty())
