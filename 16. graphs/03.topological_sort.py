8c8173f4a4532d343bcb1b3bc01a726d1412a296
AmarKanth <amarkanth.bollu@gmail.com>
2025-01-14 07:47:30 +0530

    Update 03.topological_sort.py

--- 16. graphs/03.topological_sort.py
+++ 16. graphs/03.topological_sort.py
@@ -1,6 +1,16 @@
 """
 Sorts given actions in such a way that if there is a dependency of one action on another, 
 then the dependent action always comes later than its parent action
+
+                A         B
+                  \    /     \
+                    C         D
+                  /          /
+                /          /
+               E         /
+             /  \      /            
+           /      \  /
+          H         F--------G
 """
 from collections import defaultdict
 
