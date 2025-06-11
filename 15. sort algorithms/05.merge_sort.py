31de37941162a64b7d45f373fbb060605212bc83
AmarKanth <amarkanth.bollu@gmail.com>
2025-06-11 16:49:08 +0530

    Update 05.merge_sort.py

--- 15. sort algorithms/05.merge_sort.py
+++ 15. sort algorithms/05.merge_sort.py
@@ -46,12 +46,12 @@ Note : Refer Big O Notaion for T(N/2) and O(NLogN)
 """
 def merge_sort(custom_list, l, r):
     if l < r:
-        m = (l+(r-1))//2
+        m = (l+r)//2
         merge_sort(custom_list, l, m)
         merge_sort(custom_list, m+1, r)
         merge(custom_list, l, m, r)
     return custom_list
 
 clist = [2,1,7,6,5,3,4,9,8]
-res = merge_sort(clist, 0, 8)
+res = merge_sort(clist, 0, len(clist)-1)
 print(res)
