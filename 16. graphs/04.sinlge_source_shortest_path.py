28379da697268059475d4b2ee1b2594a3b0140b4
AmarKanth <amarkanth.bollu@gmail.com>
2025-08-02 15:50:54 +0530

    Update 04.sinlge_source_shortest_path.py

--- 16. graphs/04.sinlge_source_shortest_path.py
+++ 16. graphs/04.sinlge_source_shortest_path.py
@@ -1,15 +1,14 @@
 """
 Single Source Shortest path means path from given vertex to all other vertices in the graph.
 """
-from queue import Queue
 
 """
 Breadth First Search
 """
+from queue import Queue
+
 class Graph:
-    def __init__(self, gdict=None):
-        if gdict is None:
-            gdict = {}
+    def __init__(self, gdict):
         self.gdict = gdict
     
     def print_graph(self):
@@ -17,7 +16,6 @@ class Graph:
             print(vertex,":",edges)
     
     """
-    Note : It visits only connected nodes
     TimeComplexity: O(E)
     SpaceComplexity: O(E)
     """
@@ -34,8 +32,7 @@ class Graph:
                 new_path.append(adjacent)
                 q.put(new_path)
 
-# If we change the order of edges in vertex shortest path will change
-customDict = {
+custom_dict = {
     "A": ["B", "C"],
     "B": ["D", "G"],
     "C": ["D", "E"],
@@ -44,7 +41,7 @@ customDict = {
     "G": ["F"]
 }
 
-graph = Graph(customDict)
+graph = Graph(custom_dict)
 print(graph.print_graph())
 print(graph.bfs("A", "F"))
 
