58601a2eb54e2873ed50ca46630fcaaa6499e0b8
AmarKanth <amarkanth.bollu@gmail.com>
2025-09-19 15:11:29 +0530

    Update 05. basics.py

--- 05. dict/05. basics.py
+++ 05. dict/05. basics.py
@@ -22,3 +22,33 @@ def flatten_list(input, res):
 
 output = flatten_list({"a": "1", "b":[1,2,3,5], "c": {"a":1, "b":[1,2,3,4]}}, [])
 print(output)
+
+"""
+Implement the LRUCache
+"""
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, size):
+        self.max_size = size
+        self.elements = OrderedDict()
+    
+    def get(self, key):
+        if key in self.elements:
+            self.elements.move_to_end(key, last=True)
+            return self.elements[key]
+        return -1
+    
+    def put(self, key, value):
+        if key in self.elements:
+            # Update value and mark as MRU
+            self.elements.move_to_end(key, last=True)
+        else:
+            if len(self.elements) >= self.max_size:
+                self.elements.popitem(last=False)
+        self.elements[key] = value
+    
+    def peek(self, key):
+        return self.elements.get(key, -1)
+    
+    def clear(self):
+        self.elements.clear()
