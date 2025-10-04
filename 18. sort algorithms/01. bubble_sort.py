ee774aa83419e41d305c10f515d676d807f38b16
AmarKanth <amarkanth.bollu@gmail.com>
2025-10-04 19:19:35 +0530

    changes in bubble sort

--- 18. sort algorithms/01. bubble_sort.py
+++ 18. sort algorithms/01. bubble_sort.py
@@ -11,3 +11,14 @@ def bubble_sort(custom_list):
 
 res = bubble_sort([0,9,7,1,2,3,6,5,4,8])
 print(res)
+
+"""
+406. Queue Reconstruction by Height
+"""
+people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+people.sort(key=lambda x: (-x[0], x[1]))
+
+ans = []
+for p in people:
+    ans.insert(p[1], p)
+print(ans)
