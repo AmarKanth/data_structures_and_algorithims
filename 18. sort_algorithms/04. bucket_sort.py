5f3860879bf79c769ae0bf8edd3d1a5d82f0e1bf
AmarKanth <amarkanth.bollu@gmail.com>
2025-11-02 18:53:38 +0530

    changes in bucket sort

--- 18. sort_algorithms/04. bucket_sort.py
+++ 18. sort_algorithms/04. bucket_sort.py
@@ -42,3 +42,113 @@ def bucket_sort(custom_list):
 
 res = bucket_sort([2,1,5,3,9,7,4,6,8])
 print(res)
+
+"""
+455. Assign Cookies
+"""
+def find_content_children(g, s):
+    g.sort()
+    s.sort()
+    i = 0
+    j = 0
+    content = 0
+
+    while i < len(g) and j < len(s):
+        if s[j] >= g[i]:
+            content += 1
+            i += 1
+            j += 1
+        else:
+            j += 1
+    return content
+
+result = find_content_children([1,2], [1,2,3])
+print(result)
+
+"""
+88. Merge Sorted Array
+"""
+def merge(nums1, m, nums2, n):
+    p1 = m - 1
+    p2 = n - 1
+    p = m + n - 1
+
+    while p1 >= 0 and p2 >= 0:
+        if nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+
+    while p2 >= 0:
+        nums1[p] = nums2[p2]
+        p2 -= 1
+        p -= 1
+
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+merge(nums1, 3, nums2, 3)
+print(nums1)
+
+"""
+1005. Maximize Sum Of Array After K Negations
+"""
+def largest_sum_after_k_negations(nums, k):
+    nums.sort()
+    n = len(nums)
+
+    i = 0
+    while i < n and k > 0 and nums[i] < 0:
+        nums[i] = -nums[i]
+        i += 1
+        k -= 1
+
+    total = sum(nums)
+    min_abs = min(abs(x) for x in nums)
+    if k % 2 == 1:
+        total -= 2 * min_abs
+    return total
+
+result = largest_sum_after_k_negations([4,2,3], 1)
+print(result)
+
+"""
+747. Largest Number At Least Twice of Others
+"""
+def dominant_index(nums):
+    if len(nums) == 1:
+        return 0
+
+    max_num = max(nums)
+    max_index = nums.index(max_num)
+
+    second_max = max(x for x in nums if x != max_num)
+
+    if max_num >= 2 * second_max:
+        return max_index
+    else:
+        return -1
+
+result = dominant_index([3,6,1,0])
+print(result)
+
+"""
+645. Set Mismatch
+"""
+def find_error_nums(nums):
+    seen = set()
+    duplicate = -1
+
+    for num in nums:
+        if num in seen:
+            duplicate = num
+        seen.add(num)
+
+    n = len(nums)
+    missing = (set(range(1, n + 1)) - seen).pop()
+    return [duplicate, missing]
+
+result = find_error_nums([1,2,2,4])
+print(result)
